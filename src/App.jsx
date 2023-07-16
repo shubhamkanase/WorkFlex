d6504c4440d896ca9020aada50b8220e86b833c4
shubham kanase <shubhamkanase92@gmail.com>
2023-07-16 20:50:59 +0530

    add some code

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import React from "react"; "react";
+import Navbar from "./components/navbar/Navbar";
+
+
 function App() {
 
   return (
     <div>
-      Hello world! <br />
-        jiohujgu grhgregh
+      <Navbar/>
     </div>
   )
 }
