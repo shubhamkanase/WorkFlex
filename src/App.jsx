8f7db82cf80e92cae7ca864d6431f9dc72e95c47
shubham kanase <shubhamkanase92@gmail.com>
2023-07-17 13:19:37 +0530

    add navbar scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react"; "react";
+import React from "react";
 import Navbar from "./components/navbar/Navbar";
 
 
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,7 +3,22 @@ import "./Navbar.scss"
 
 const Navbar = () => {
   return (
-    <div className='navbar'>Navbar</div>
+    <div className='navbar'>
+      <div className="container">
+        <div className="logo">
+         <img className="logo" src="./img/WorkFlexlogo.png" alt="" />
+        </div>
+        <div className="link">
+          <spna>WorkFlex Bussiness</spna>
+          <spna>Explore</spna>
+          <spna>English</spna>
+          <spna>Sign in</spna>
+          <spna>Become a Seller</spna>
+          <button>Join</button>
+        </div>
+
+      </div>
+    </div>
   )
 }
 
