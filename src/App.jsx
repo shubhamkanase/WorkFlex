280fbfe411a1c35d2021a7c34faea089cc04fce4
shubham kanase <shubhamkanase92@gmail.com>
2023-07-17 23:46:24 +0530

    update some code

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,39 @@
 import React from "react";
 import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
+import Home from "./pages/home/Home";
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet
+} from "react-router-dom";
 
 function App() {
 
-  return (
-    <div>
+  const Layout = () => {
+    return (
+      <div className ="app" >
       <Navbar/>
-    </div>
-  )
+      <Outlet/>
+      <Footer/>
+      </div >
+    )
+  };
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>,
+    
+  },
+]);
+
+return (
+  <div>
+    <RouterProvider router={router} />
+  </div>
+);
 }
 
 export default App
