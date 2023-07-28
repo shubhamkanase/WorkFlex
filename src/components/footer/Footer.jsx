f25359b46f8b720733b1226f331b7acfa82e486f
shubham kanase <shubhamkanase92@gmail.com>
2023-07-28 23:37:58 +0530

    Add some functionality

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,7 +3,84 @@ import "./Footer.scss"
 
 const Footer = () => {
   return (
-    <div className='footer'>Footer</div>
+    <div className='footer'>Footer
+
+   
+<div>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+      Eveniet quisquam itaque animi reprehenderit dolorum nemo quidem facilis qui deleniti commodi?</div>
+      <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+      Eveniet quisquam itaque animi reprehenderit dolorum nemo quidem facilis qui deleniti commodi?</div>
+      <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+      Eveniet quisquam itaque animi reprehenderit dolorum nemo quidem facilis qui deleniti commodi?</div>
+      <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+      Eveniet quisquam itaque animi reprehenderit dolorum nemo quidem facilis qui deleniti commodi?</div>
+      <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+      Eveniet quisquam itaque animi reprehenderit dolorum nemo quidem facilis qui deleniti commodi?</div>
+      <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+      Eveniet quisquam itaque animi reprehenderit dolorum nemo quidem facilis qui deleniti commodi?</div>
+      <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+      Eveniet quisquam itaque animi reprehenderit dolorum nemo quidem facilis qui deleniti commodi?</div>
+      <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+      Eveniet quisquam itaque animi reprehenderit dolorum nemo quidem facilis qui deleniti commodi?</div>
+      <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+      Eveniet quisquam itaque animi reprehenderit dolorum nemo quidem facilis qui deleniti commodi?</div>
+      <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+      Eveniet quisquam itaque animi reprehenderit dolorum nemo quidem facilis qui deleniti commodi?</div>
+      <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+      Eveniet quisquam itaque animi reprehenderit dolorum nemo quidem facilis qui deleniti commodi?</div>
+      <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+      Eveniet quisquam itaque animi reprehenderit dolorum nemo quidem facilis qui deleniti commodi?</div>
+      <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+      Eveniet quisquam itaque animi reprehenderit dolorum nemo quidem facilis qui deleniti commodi?</div>
+      
+      <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+      Eveniet quisquam itaque animi reprehenderit dolorum nemo quidem 
+      facilis qui deleniti commodi?</div>
+      <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+      Eveniet quisquam itaque animi reprehenderit dolorum nemo quidem 
+      facilis qui deleniti commodi?</div>
+      <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+      Eveniet quisquam itaque animi reprehenderit dolorum nemo quidem 
+      facilis qui deleniti commodi?</div>
+      <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+      Eveniet quisquam itaque animi reprehenderit dolorum nemo quidem 
+      facilis qui deleniti commodi?</div>
+      <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+      Eveniet quisquam itaque animi reprehenderit dolorum nemo quidem 
+      facilis qui deleniti commodi?</div><div>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+      Eveniet quisquam itaque animi reprehenderit dolorum nemo quidem 
+      facilis qui deleniti commodi?</div><div>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+      Eveniet quisquam itaque animi reprehenderit dolorum nemo quidem 
+      facilis qui deleniti commodi?</div><div>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+      Eveniet quisquam itaque animi reprehenderit dolorum nemo quidem 
+      facilis qui deleniti commodi?</div><div>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+      Eveniet quisquam itaque animi reprehenderit dolorum nemo quidem 
+      facilis qui deleniti commodi?</div><div>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+      Eveniet quisquam itaque animi reprehenderit dolorum nemo quidem 
+      facilis qui deleniti commodi?</div><div>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+      Eveniet quisquam itaque animi reprehenderit dolorum nemo quidem 
+      facilis qui deleniti commodi?</div><div>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+      Eveniet quisquam itaque animi reprehenderit dolorum nemo quidem 
+      facilis qui deleniti commodi?</div><div>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+      Eveniet quisquam itaque animi reprehenderit dolorum nemo quidem 
+      facilis qui deleniti commodi?</div><div>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+      Eveniet quisquam itaque animi reprehenderit dolorum nemo quidem 
+      facilis qui deleniti commodi?</div>
+      <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+      Eveniet quisquam itaque animi reprehenderit dolorum nemo quidem 
+      facilis qui deleniti commodi?</div><div>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+      Eveniet quisquam itaque animi reprehenderit dolorum nemo quidem 
+      facilis qui deleniti commodi?</div><div>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+      Eveniet quisquam itaque animi reprehenderit dolorum nemo quidem 
+      facilis qui deleniti commodi?</div><div>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+      Eveniet quisquam itaque animi reprehenderit dolorum nemo quidem 
+      facilis qui deleniti commodi?</div><div>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+      Eveniet quisquam itaque animi reprehenderit dolorum nemo quidem 
+      facilis qui deleniti commodi?</div>
+
+    </div>
+
+    
   )
 }
 
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,25 +3,25 @@ import "./Navbar.scss"
 import { Link, useLocation } from 'react-router-dom'
 
 const Navbar = () => {
-  const [active,setActive] = useState(false);
-  const [open,setOpen] = useState(true);
+  const [active, setActive] = useState(false);
+  const [open, setOpen] = useState(true);
 
-  const {pathname} = useLocation()
+  const { pathname } = useLocation()
 
-  const isActive = () =>{
-    Window.srollY > 0 ? setActive(true): setActive(false)
+  const isActive = () => {
+    Window.srollY > 0 ? setActive(true) : setActive(false);
   };
 
-  useEffect (() => {
+  useEffect(() => {
     window.addEventListener('scroll', isActive);
-    return()=>{
-      window.removeEventListener('scroll',isActive)
+    return () => {
+      window.removeEventListener('scroll', isActive)
     }
-  },[]);
+  }, []);
 
 
-  
-  
+
+
   const currentUser = {
     id: 1,
     username: "Shubham Kanase",
@@ -33,7 +33,7 @@ const Navbar = () => {
       <div className="container">
         <div className="logo">
           <Link>
-          <img className="logo" src="./img/WorkFlexlogo.png" alt="" />
+            <img className="logo" src="./img/WorkFlexlogo.png" alt="" />
           </Link>
         </div>
         <div className="link">
@@ -44,37 +44,66 @@ const Navbar = () => {
           {!currentUser?.isSeller && <spna>Become a Seller</spna>}
           {!currentUser && <button>Join</button>}
           {currentUser && (
-            <div className="user" onClick={()=>setOpen(!open)}>
+            <div className="user" onClick={() => setOpen(!open)}>
               <img src="./img/Photograph.jpg" alt="" />
               <span>{currentUser?.username}</span>
               {!open && <div className="options">
-               {
-                currentUser?.isSeller && (
-                  <>
-                   <Link to="/mygigs">Gigs</Link>
-                   <Link to="/add">Add New Gig</Link>
-                  </>
-                )}
-                 <Link to="/orders">Orders</Link>
-                 <Link to="/messages">Messages</Link>
-                 <Link to="/">Logout</Link>
-            </div>}
-          </div>
-         )}
-      </div>
+                {
+                  currentUser?.isSeller && (
+                    <>
+                      <Link to="/mygigs">Gigs</Link>
+                      <Link to="/add">Add New Gig</Link>
+                    </>
+                  )}
+                <Link to="/orders">Orders</Link>
+                <Link to="/messages">Messages</Link>
+                <Link to="/">Logout</Link>
+              </div>}
+            </div>
+          )}
+        </div>
 
-    </div>
-     {(active || pathname !== "/") && (
-      <>
-      <hr/>
-      <div className="menu">
-        <span>test</span>
-        <span>test2</span>
       </div>
-      </>
-     )}
+      {(active || pathname !== "/") && (
+        <>
+          <hr />
+          <div className='menuItems'>
+          <div className="menu">
+            <Link className='links' to="/">
+              Graphic & Design
+            </Link>
+            <Link className='links' to="/">
+              Video & Animation
+            </Link>
+            <Link className='links' to="/">
+              Writing & Translation
+            </Link>
+            <Link className='links' to="/">
+              AI Services
+            </Link>
+            <Link className='links' to="/">
+              Digital & Marketing
+            </Link>
+            <Link className='links' to="/">
+              Music & Audio
+            </Link>
+            <Link className='links' to="/">
+              Programming & Tech
+            </Link>
+            <Link className='links' to="/">
+              Business
+            </Link>
+            <Link className='links' to="/">
+              Lifestyle
+            </Link>
+          </div>
+          </div>
+          
+        </>
+      )}
     </div >
   )
 }
+
 
 export default Navbar
