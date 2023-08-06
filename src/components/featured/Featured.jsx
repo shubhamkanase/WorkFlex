5371371eb046c671fdabb7444fd6cc8fbe442785
shubham kanase <shubhamkanase92@gmail.com>
2023-08-06 21:00:03 +0530

    add  some functionality

---
+++ src/components/featured/Featured.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import "./Featured.scss"
+
+const Featured = () => {
+  return (
+    <div className='Featured'>
+        <div className="Container">
+            <div />
+            <div className="left">
+                <h1>Find the perfect Freelancing service for your Bussiness</h1>
+                <div className="search">
+                    <div className="searchInput">
+                        <img src="" alt="" />
+                        <input type="text" placeholder='try your bussiness ' />
+
+                    </div>
+                    <button>Search</button>
+                </div>
+
+            </div>
+            <div />
+            <div className="right">
+
+            </div>
+
+        </div>
+    </div>
+  )
+}
+
+export default Featured
