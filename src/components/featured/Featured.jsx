78964bdfb409e6e5a6eba36750b09fbb380e9db9
shubham kanase <shubhamkanase92@gmail.com>
2023-08-07 22:53:48 +0530

    install npm update version

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -10,16 +10,25 @@ const Featured = () => {
                 <h1>Find the perfect Freelancing service for your Bussiness</h1>
                 <div className="search">
                     <div className="searchInput">
-                        <img src="" alt="" />
+                        <img src="./img/searchicon.png" alt="" />
                         <input type="text" placeholder='try your bussiness ' />
 
                     </div>
                     <button>Search</button>
                 </div>
+                <div className="popular">
+                    <span>Popular:</span>
+                    <button>Web Design</button> 
+                    <button>Wordpress</button> 
+                    <button>Logo Design</button>
+                    <button>AI Services</button>
+                </div>
 
             </div>
             <div />
             <div className="right">
+                <img className='homePic' src="./img/homeimg1.png" alt="" />
+                <img className='homepic2'src="./img/homeimg2.svg" alt="" />
 
             </div>
 
