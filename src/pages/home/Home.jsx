4a0f7764c403de4174e76f84d54b7e521ea565b0
shubham kanase <shubhamkanase92@gmail.com>
2023-08-13 19:15:54 +0530

    add some functinality

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -18,6 +18,40 @@ const Home= () => {
         ))}
 
       </Slide>
+      <div className="features">
+        <div className="container">
+          <div className="item">
+            <h1> A whole world of freelancing talent at your fingertips</h1>
+            <div className="title">
+              <img src="./img/checkicon.png" alt="" />
+              The best for every budget
+            </div>
+            <p>
+              Find high-quality service at every price point. No Hourly rates,
+              just project-based pricing.
+            </p>
+            <div className="title">
+              <img src="./img/checkicon.png" alt="" />
+              The best for every budget
+            </div>
+            <p>
+              Find high-quality service at every price point. No Hourly rates,
+              just project-based pricing.
+            </p>
+            <div className="title">
+              <img src="./img/checkicon.png" alt="" />
+              The best for every budget
+            </div>
+            <p>
+              Find high-quality service at every price point. No Hourly rates,
+              just project-based pricing.
+            </p>
+          </div>
+          <div className="item">
+            <video src="./videos/feacturesvideo.mp4" controls></video>
+          </div>
+        </div>
+      </div>
      
     </div>
   )
