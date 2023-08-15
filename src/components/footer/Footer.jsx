95c01787dc0ac5b46f35091020c10277b3a93afc
shubham kanase <shubhamkanase92@gmail.com>
2023-08-15 13:32:41 +0530

    add some functinality

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,13 +1,107 @@
 import React from 'react'
 import "./Footer.scss"
+import { AccessibilityNew, AttachMoney, Copyright, Facebook, Instagram, Language, LinkedIn, Twitter } from '@mui/icons-material'
 
 const Footer = () => {
   return (
-    <div className='footer'>Footer
+    <div className='footer'>
+      <div className="container">
+        <div className="top">
+          <div className="item">
+            <h2>Categories</h2>
+            <span> Graphics & Design  span </span>
+            <span> Digital Marketing span </span>
+            <span> Writing & Translation span </span>
+            <span> Video & Animation span </span>
+            <span> Music & Audio span </span>
+            <span> Fiverr Logo Maker span </span>
+            <span> Programming & Tech span </span>
+            <span> Data span </span>
+            <span> Business span </span>
+            <span> Lifestyle span </span>
+            <span> Photography span </span>
+            <span> End-to-End Projects span </span>
+            <span> Sitemap span </span>
+          </div>
 
+          <div className="item">
+            <h2>About</h2>
+            <span> Careers </span>
+            <span> Press & News </span>
+            <span> Partnerships </span>
+            <span> Privacy Policy </span>
+            <span> Terms of Service </span>
+            <span> Intellectual Property Claims </span>
+            <span> Investor Relations </span>
+          </div>
+
+          <div className="item">
+            <h2>Support and Education</h2>
+            <span> Help & Support </span>
+            <span> Trust & Safety </span>
+            <span> Selling on Fiverr </span>
+            <span> Buying on Fiverr </span>
+            <span> Fiverr Guides </span>
+            <span>  </span>
+          </div>
+
+          <div className="item">
+            <h2>Community</h2>
+            <span> Customer Success Stories </span>
+            <span> Community Hub </span>
+            <span> Forum </span>
+            <span> Events </span>
+            <span> Blog </span>
+            <span> Influencers </span>
+            <span> Affiliates </span>
+            <span> Podcast </span>
+            <span> Invite a Friend </span>
+            <span> Become a Seller </span>
+            <span> Community Standards </span>
+          </div>
+
+          <div className="item">
+            <h2>Business Solutions</h2>
+            <span> About Business Solutions </span>
+            <span> Fiverr Pro </span>
+            <span> Fiverr Certified </span>
+            <span> Fiverr Enterprise </span>
+            <span> Working Not Working </span>
+            <span> Contact Sales </span>
+         </div>
+        </div>
+        <hr />
+        <div className="bottom">
+          <div className="left">
+            <h1>WorkFlex</h1>
+            <span> <Copyright fontSize='small'/> WorkFlex International Ltd. 2023</span>
+
+          </div>
+          <div className="right">
+            <div className="social">
+              <Twitter className='twitcolor' fontSize='large' />
+              <Facebook className='facecolor' fontSize='large'/>
+              <LinkedIn className='linkcolor' fontSize='large'/>
+              <Instagram className='instacolor' fontSize='large'/>
+            </div>
+            <div className="link">
+              <Language className='langcolor' fontSize='large'/>
+              <span>English</span>
+            </div>
+            <div className="link">
+              <AttachMoney className='attachcolor'fontSize='large' />
+              <span>USD</span>
+            </div>
+            <div className="link">
+              <AccessibilityNew className='access' fontSize='large'/>
+            </div>
+
+          </div>
+        </div>
+      </div>
     </div>
 
-    
+
   )
 }
 
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -6,6 +6,9 @@ import TrustedBy from '../../components/trustedBy/TrustedBy'
 import Slide from '../../components/Slide/Slide'
 import { cards } from "../../data"
 import CatCard from '../../components/catCard/CatCard'
+import { projects } from '../../data'
+import ProjectCard from '../../components/projectCard copy/ProjectCard'
+import { CheckCircleOutline } from '@mui/icons-material'
 
 const Home = () => {
   return (
@@ -27,23 +30,31 @@ const Home = () => {
               The best for every budget
             </div>
             <p>
-              Find high-quality service at every price point. No Hourly rates,
+              Find high-quality services at every price point. No hourly rates,
               just project-based pricing.
             </p>
             <div className="title">
               <img src="./img/checkicon.png" alt="" />
-              The best for every budget
+              Quality work done quickly
             </div>
             <p>
-              Find high-quality service at every price point. No Hourly rates,
-              just project-based pricing.
+              Find the right freelancer to begin working on your project within
+              minutes.
             </p>
             <div className="title">
               <img src="./img/checkicon.png" alt="" />
-              The best for every budget
+              Protected payments, every time
             </div>
             <p>
-              Find high-quality service at every price point. No Hourly rates,
+              Always know what you'll pay upfront. Your payment isn't released
+              until you approve the work.
+            </p>
+            <div className="title">
+              <img src="./img/checkicon.png" alt="" />
+              24/7 support
+            </div>
+            <p>
+              Find high-quality services at every price point. No hourly rates,
               just project-based pricing.
             </p>
           </div>
@@ -60,15 +71,15 @@ const Home = () => {
               dedicated to businesses</p>
 
             <div className="title">
-              <img src="./img/checkicon.png" alt="" />
-              Connect to freelancers with proven business experience
+              <CheckCircleOutline className='checkicon' sx={{ fontSize: 30 }}/>
+              Connect to freelancers with proven business experience 
             </div>
             <div className="title">
-              <img src="./img/checkicon.png" alt="" />
-             Get matched with the perfect talent by a customer success manager
+            <CheckCircleOutline className='checkicon' sx={{ fontSize: 30 }}/>
+              Get matched with the perfect talent by a customer success manager
             </div>
             <div className="title">
-              <img src="./img/checkicon.png" alt="" />
+            <CheckCircleOutline className='checkicon' sx={{ fontSize: 30 }}/>
               Manage teamwork work and boost productivity with one powerful workspace
             </div>
             <button className='btnexplore'>Explore WorkFlex Bussiness</button>
@@ -86,6 +97,12 @@ const Home = () => {
           </div>
         </div>
       </div>
+
+      <Slide slidesToShow={4} arrowsSrcoll={4}>
+        {projects.map(card => (
+          <ProjectCard key={card.id} item={card} />
+        ))}
+      </Slide>
 
 
     </div>
