e486192a4025d1206bc07f64341465e152bdfa58
shubham kanase <shubhamkanase92@gmail.com>
2023-08-20 23:25:15 +0530

    update MyGig

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Home from "./pages/home/Home";
 import Gigs from "./pages/gigs/Gigs"
 import Gig from "./pages/gig/Gig"
 import Orders from "./pages/orders/Orders"
-import MyGigs from "./pages/mygigs/MyGigs"
+import MyGigs from "./pages/myGigs/MyGigs"
 import Add from "./pages/add/Add"
 import Messages from "./pages/messages/Messages"
 import Message from "./pages/message/Message"
--- src/pages/myGigs/MyGigs.jsx
+++ src/pages/myGigs/MyGigs.jsx
@@ -1,9 +1,113 @@
 import React from 'react'
 import "./MyGigs.scss"
+import { Delete } from '@mui/icons-material'
+import { Link } from 'react-router-dom'
 
 const MyGigs = () => {
   return (
-    <div className='myGigs'>MyGigs</div>
+    <div className='myGigs'>
+      <div className="container">
+        <div className="title">
+          <h1>Gigs</h1>
+          <Link to='/add'>
+            <button>Add New Gig</button>
+            </Link>
+        </div>
+        <table>
+          <tr>
+            <th>Image</th>
+            <th>Title</th>
+            <th>Price</th>
+            <th>Sales</th>
+            <th>Action</th>
+          </tr>
+          <tr>
+            <td> <img className='image' src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" /></td>
+            <td>Stunning concept art</td>
+            <td>59.<sup>99</sup></td>
+            <td>13</td>
+            <td> <Delete className='delete'/></td>
+          </tr>
+
+          <tr>
+            <td>
+              <img
+                className="image"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td>Ai generated concept art</td>
+            <td>120.<sup>99</sup></td>
+            <td>41</td>
+            <td>
+               <Delete/>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="image"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td>High quality digital character</td>
+            <td>79.<sup>99</sup></td>
+            <td>55</td>
+            <td>
+              <Delete/>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="image"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td>Illustration hyper realistic painting</td>
+            <td>119.<sup>99</sup></td>
+            <td>29</td>
+            <td>
+              <Delete/>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="image"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td>Original ai generated digital art</td>
+            <td>59.<sup>99</sup></td>
+            <td>34</td>
+            <td>
+            <Delete/>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="image"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td>Text based ai generated art</td>
+            <td>110.<sup>99</sup></td>
+            <td>16</td>
+            <td>
+            <Delete/>
+            </td>
+          </tr>
+
+        </table>
+      </div>
+    </div>
   )
 }
   
