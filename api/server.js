c2a6349e520b16617f13e8a4ed9f24962b1f2ea5
shubham kanase <shubhamkanase92@gmail.com>
2023-09-05 23:03:09 +0530

    connect express to mongodb

--- api/server.js
+++ api/server.js
@@ -1,1 +1,16 @@
-console.log("shubham")
+import Express from "express";
+import mongoose from "mongoose";
+
+const app = Express()
+mongoose.set('strictQuery', true)
+try {
+    await mongoose.connect('mongodb+srv://shubhamkanase:shubhamsk@cluster0.rmw4sow.mongodb.net/?retryWrites=true&w=majority&dbname=WorkFlex');
+    console.log("mongodb connected")
+  } catch (error) {
+    handleError(error);
+  }
+
+
+app.listen(8800, () => {
+    console.log("running bachend server");
+})
