a58856fbef42d9e43aaafff4646d2010509a3fcd
Shubham Kanase <shubhamkanase92@gmail,com>
2024-04-23 22:39:56 +0530

    update

--- client/src/utils/newRequest.js
+++ client/src/utils/newRequest.js
@@ -1,11 +1,7 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-<<<<<<< HEAD
-  baseURL: "http://localhost:8800/api/",
-=======
   baseURL: "http://localhost:8800/api",
->>>>>>> 494744736fccee31f0401ce2d804d18c4b4e1da7
   withCredentials: true,
 });
 
